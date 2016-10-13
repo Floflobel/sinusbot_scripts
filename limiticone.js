05b0437ea2a55cbc97addb635330ac692ecdd3a1
Floflobel <floflobel@hotmail.fr>
2016-10-14 01:55:07 +0200

    add while for count the number of groups and message if number > limit

--- limiticone.js
+++ limiticone.js
@@ -1,29 +1,68 @@
 registerPlugin({
-  name: 'LimitIcone',
-  version: '1.0',
-  description: 'Limit icone',
-  author: 'Floflobel',
-  vars: {
-    limit: {
-      title: 'Set the limit of Server Groups',
-      type: 'number',
-      placeholder: "Max ?"
-    },
-    option: {
-      title: 'Select the punition',
-      type: 'select',
-      options: [
-        'Message',
-        'Poke'
-      ]
-    },
-  }
-}, function(sinusbot, config) {
-    sinusbot.on('clientMove', function(ev) {
-      var test = getServerGroups()
-	  if(ev.oldChannel == 0) {
-			sinusbot.log("'" + ev.clientNick + "' is connected. Checking for the server groups...");
-			sinusbot.log("ServerGroup : " + test + ".");
-	  }
-  });
-});
+	name: 'LimitIcone',
+	version: '1.0',
+	description: 'Limit icone',
+	author: 'Floflobel',
+	vars: {
+		limit: {
+			title: 'Set the limit of Server Groups',
+			type: 'number',
+			placeholder: "5"
+		}
+		optionmessage: {
+			title: 'Select the type of prevent',
+			type: 'select',
+			options: [
+				'No Message',
+				'Message',
+				'Poke (max. 100 characters)'
+			]
+		}
+		message: {
+			title: 'Set the message for prevent',
+			type: 'string',
+			placeholder: ""
+		}
+		punition: {
+			title: 'Select the punition',
+			type: 'select',
+			options: [
+				'Remove lastest group',
+				'Kick',
+				'Ban'
+			]
+		}
+		timetopunition: {
+			title: 'Set the time to punition is applie (seconds)',
+			type: 'number',
+			placeholder: "60"
+		}
+	}
+}, 	function(sinusbot, config) {
+		sinusbot.on('clientMove', function(ev) {
+			var srvgroups = ev.clientServerGroups
+			if(ev.oldChannel == 0) {
+				var count = 0;
+				for (var k in srvgroups) {
+					if (srvgroups.hasOwnProperty(k)) {
+						++count;
+					}
+				}
+			sinusbot.log("'" + ev.clientNick + "' is connected. Number of groups: " + count);
+			}
+			
+			if(count > config.limit) {	
+				if (config.optionmessage == 0) {
+					return;
+				}
+				else if (config.optionmessage == 1) {
+					sinusbot.chatPrivate(ev.clientId, config.message)
+				}
+				else if (config.optionmessage == 2) {
+					message = config.message.substring(0, 100);
+					sinusbot.poke(ev.clientId, message);
+				}
+				
+			}
+		});
+	});
