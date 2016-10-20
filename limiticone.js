b5474c189aa6ef930457332173ab77a3693e5327
Floflobel <floflobel@hotmail.fr>
2016-10-21 00:59:45 +0200

    bu

--- limiticone.js
+++ limiticone.js
@@ -105,7 +105,7 @@ registerPlugin({
 						}
 						else {
 							//sinusbot.log('Debug remove : ' + ev.clientId + ' - ' + removegroup)
-							sinusbot.removeClientFromServerGroup(ev.clientIdi.dbId, removegroup)
+							//sinusbot.removeClientFromServerGroup(ev.client.dbId, removegroup);
 						}
 					}
 				}
