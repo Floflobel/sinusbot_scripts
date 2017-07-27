71a193dd596b2490b361eecf330996e0a791350d
Floflobel <floflobel@hotmail.fr>
2017-07-27 18:09:44 +0200

    initial commit

---
+++ H1Z1KOTHServerStatus.js
@@ -0,0 +1,71 @@
+registerPlugin({
+	name: 'H1Z1 KOTH Server Information',
+	version: '1.0.0',
+	description: 'Shows information about most steam player on a channel description or via privat chat.',
+	author: 'Floflobel',
+	vars: [
+		{
+			name: 'apiKey',
+			title: 'Your personal API Key - check the installation instructions if you dont know how to get one',
+			type: 'string'
+		}, 
+		{
+			name: 'interval',
+			title: 'Update Interval in minutes',
+			indent: 2,
+			type: 'number',
+			placeholder: 5
+		},
+	]
+   
+}, function(sinusbot, config) {
+	var event = require('event');
+	var store = require('store');
+    var backend = require('backend');
+    var engine = require('engine');
+	
+	
+	interval = config.interval * 60000;
+    if (interval < 60000) {
+        interval = 60000;
+    }
+	
+	// if (!(config.apiKey)){
+        // engine.log('No API key found - stop script execution!');
+        // return;
+    // }
+	
+
+	queryData()
+	setInterval(queryData, interval);
+	
+	function queryData() {
+        sinusbot.http({
+          "method": "GET",
+          "url":    'http://census.daybreakgames.com/json/status?game=h1z1xx',
+          "timeout": 6000,
+          "headers": [
+            {"Content-Type": "application/json"}
+          ]
+        }, function (error, response) {
+            if (typeof response != 'undefined' && response.statusCode == 200) {
+                var serverInformation = JSON.parse(response.data);
+                if (typeof serverInformation != 'undefined') { 
+                    if (typeof serverInformation.error != 'undefined') {
+                        engine.log("API Error: " + serverInformation.error);
+                    }
+                    processData(serverInformation);
+                } else {
+              engine.log("ERROR: invalid response: " + response.data);
+            }
+          }
+        });
+    }
+	
+	function processData(serverInformation){
+		var duos1 = 'serverInformation.h1z1xx.Europe."Duos 1 (EU)".status';
+		
+		engine.log(duos1);
+    }
+
+});
