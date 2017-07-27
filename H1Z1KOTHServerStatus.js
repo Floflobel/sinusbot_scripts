6913bf1f012622e19fe94ff51cad779118ba7eda
Floflobel <floflobel@hotmail.fr>
2017-07-27 21:31:12 +0200

    bu

--- H1Z1KOTHServerStatus.js
+++ H1Z1KOTHServerStatus.js
@@ -7,29 +7,70 @@ registerPlugin({
 		{
 			name: 'interval',
 			title: 'Update Interval in minutes',
-			//indent: 2
+			indent: 2
 			type: 'number',
 			placeholder: 5
 		},
 		{
-			name: 'Solo1',
+			name: 'Solo 1',
 			title: 'Solo 1',
-			//indent: 2,
+			indent: 2,
 			type: 'channel',
 		},
 		{
-			name: 'Solo2',
+			name: 'Solo 2',
 			title: 'Solo 2',
-			//indent: 2,
+			indent: 2,
 			type: 'channel',
 		},
 		{
-			name: 'Solo3',
+			name: 'Solo 3',
 			title: 'Solo 3',
-			//indent: 2,
+			indent: 2,
 			type: 'channel',
 		},
-
+		{
+			name: 'Duo 1',
+			title: 'Duo 1',
+			indent: 2,
+			type: 'channel',
+		},
+		{
+			name: 'Duo 2',
+			title: 'Duo 2',
+			indent: 2,
+			type: 'channel',
+		},
+		{
+			name: 'Duo 3',
+			title: 'Duo 3',
+			indent: 2,
+			type: 'channel',
+		},
+		{
+			name: 'Five 1',
+			title: 'Five 1',
+			indent: 2,
+			type: 'channel',
+		},
+		{
+			name: 'Five 2',
+			title: 'Five 2',
+			indent: 2,
+			type: 'channel',
+		},
+		{
+			name: 'Five 3',
+			title: 'Five 3',
+			indent: 2,
+			type: 'channel',
+		},
+		{
+			name: 'Training',
+			title: 'Training',
+			indent: 2,
+			type: 'channel',
+		},
 	]
    
 }, function(sinusbot, config) {
@@ -38,8 +79,8 @@ registerPlugin({
     var backend = require('backend');
     var engine = require('engine');
 	
-	//var nameGame = ['Solo 1', 'Solo 2', 'Solo 3', 'Duos 1', 'Duos 2', 'Duos 3', 'Five 1', 'Five 2', 'Five 3', 'Training'];
-	var nameGame = ['Solo 1', 'Solo 2', 'Solo 3'];
+	var nameGame = ['Solo 1', 'Solo 2', 'Solo 3', 'Duos 1', 'Duos 2', 'Duos 3', 'Five 1', 'Five 2', 'Five 3', 'Training'];
+	//var nameGame = ['Solo 1', 'Solo 2', 'Solo 3'];
 	
 	interval = config.interval * 60000;
     if (interval < 60000) {
@@ -79,33 +120,12 @@ registerPlugin({
     }
 	
 	function processData(sinusbot, serverInformation){
-		// var channel = backend.getChannelByID('132');
-		// channel.setName('patate');
-		
 		for (var i in nameGame) {
 			var channel = backend.getChannelByID(config[nameGame[i]]);
 			var query = serverInformation.h1z1xx.Europe[nameGame[i] + " (EU)"]["status"];
 			var name = nameGame[i] + " (EU)" + " > " + query;
 			channel.setName(name);
 		}
-		
-		
-		
-		//var duos1_status = serverInformation.h1z1xx.Europe["Duos 1 (EU)"]["status"];
-		//var duos1_live = serverInformation.h1z1xx.Europe["Duos 1 (EU)"]["ageSeconds"];
-		
-		//backend.createChannel({ name: 'Duos 1 (EU) > ' + duos1_status + ' > ' + duos1_live + 's', 
-								//parent: config.Channel, 
-								//permanent: true, 
-								//codec: parseInt(ChannelCodec), 
-								//codecQuality: parseInt(parseInt(ChannelQuality) + 1), 
-								//maxClients: 0, 
-								//description: ChannelDescription, 
-								//neededTalkPower: parseInt(ChannelTalkPower)
-								//});
-		
-		//engine.log(duos1_status);
-		//engine.log(duos1_live);
     }
 
 });
