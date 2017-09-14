fa641f3d32efa3bf0720d87026e8170a83fdbb15
Floflobel <floflobel@hotmail.fr>
2017-09-14 21:34:33 +0200

    remove config an change command

--- come.js
+++ come.js
@@ -3,32 +3,12 @@ registerPlugin({
     version: '1.2.0',
     backends: ['ts3'],
     engine: '>= 0.9.18',
-    description: 'Implements the !come and !goaway command',
+    description: 'Implements the !come and !back command',
     author: 'Xuxe <julian@julian-huebenthal.de> & maxibanki <max@schmitt.mx>',
     vars: [{
         name: 'defaultChannel',
-        title: 'Name or ID of the default channel (needed for !goaway)',
+        title: 'Name or ID of the default channel (needed for !back)',
         type: 'channel'
-    }, {
-        name: 'clientUIDs',
-        title: 'Allowed clients',
-        type: 'array',
-        vars: [{
-            name: 'clientUID',
-            title: 'Client Unique ID',
-            type: 'string',
-            placeholder: '+q726hsF9s4u/npIV0EslIZVqAw='
-        }]
-    }, {
-        name: 'groupIDs',
-        title: 'Allowed groups',
-        type: 'array',
-        vars: [{
-            name: 'groupID',
-            title: 'Group ID',
-            type: 'string',
-            placeholder: '12'
-        }]
     }]
 }, function (sinusbot, config) {
     var engine = require('engine');
@@ -39,45 +19,8 @@ registerPlugin({
 
     event.on('chat', function (ev) {
         if (!ev.client.isSelf()) {
-
-            var isInServerGroupList = false;
-            var isInClientList = false;
-            if (ev.text === '!goaway' || ev.text === '!come') {
-                /**
-                 * Checking if the current client has the permission to
-                 * come by looping through the list of clients in the config
-                 */
-                if (config.clientUIDs) {
-                    var clientUID = ev.client.uniqueID();
-                    config.clientUIDs.some(function (e) {
-                        if (e.clientUID === clientUID) {
-                            isInClientList = true;
-                            return true;
-                        }
-                    });
-                }
-                /**
-                 * Checking if the current client has a servergroup of the
-                 * servergroups which are given in the config by looping through
-                 * both arrays
-                 */
-                if (config.groupIDs) {
-                    ev.client.getServerGroups().some(function (group) {
-                        var serverGroupID = group.id();
-                        var found = config.groupIDs.some(function (e) {
-                            if (e.groupID === serverGroupID) {
-                                isInServerGroupList = true;
-                                return true;
-                            }
-                        });
-                        if (found) {
-                            return true;
-                        }
-                    });
-                }
-            }
             switch (ev.text) {
-                case '!goaway':
+                case '!back':
                         engine.log('Going back to my default channel (' + defaultChannel.name() + ').');
                         backend.getBotClient().moveTo(defaultChannel);
                     break;
